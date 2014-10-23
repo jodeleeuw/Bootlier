2255bb9f0fac5552f113b634679f008fba80bc44
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-10-23 17:09:16 -0400

    can now id which data points are outliers

--- demo.R
+++ demo.R
@@ -4,10 +4,14 @@ source('find.h.crit.R')
 source('bootstrap.sample.R')
 source('bootstrap.sample.mtm.R')
 source('bootstrap.has.outliers.R')
+source('bootstrap.identify.outliers.R')
 
 challenger_temp_data <- c(66, 70, 69, 80, 68, 67, 72, 73, 70, 57, 63, 70, 78, 67, 53, 67, 75, 70, 81, 76, 79, 75, 76, 58, 31)
 
-bootstrap.has.outliers(challenger_temp_data)
+result <- bootstrap.identify.outliers(challenger_temp_data)
+
+
+
 
 
 
