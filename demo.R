7b46245ff8950343cb90899081062deaaf5d6630
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-10-23 17:22:09 -0400

    add plot to demo

--- demo.R
+++ demo.R
@@ -10,6 +10,10 @@ challenger_temp_data <- c(66, 70, 69, 80, 68, 67, 72, 73, 70, 57, 63, 70, 78, 67
 
 result <- bootstrap.identify.outliers(challenger_temp_data)
 
+p.data <- hist(result$data.truncated, breaks=seq(from=30, to=90, by=5))
+p.outlier <- hist(result$data.outlier.set, breaks=seq(from=30, to=90, by=5))
+plot(p.data, col = rgb(0,0,1,1/4), xlim=c(30,90))
+plot(p.outlier, col = rgb(1,0,0,1/4), xlim=c(30,90), add=T)
 
 
 
