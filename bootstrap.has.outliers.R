a4c96f09e144813489c7414adb3a208d57336b88
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-10-23 17:14:48 -0400

    add param options, update read

--- bootstrap.has.outliers.R
+++ bootstrap.has.outliers.R
@@ -1,13 +1,13 @@
-bootstrap.has.outliers <- function(data, alpha=0.05, adjust=1) {
+bootstrap.has.outliers <- function(data, bootstrap.sample.size=1000, mtm.trim.amount=2, alpha=0.05, adjust=1, h.crit.search.depth=25) {
   # bootstrap the mean - trimmed mean stat
-  M <- bootstrap.sample.mtm(data)
+  M <- bootstrap.sample.mtm(data, bootstrap.sample.size, mtm.trim.amount)
   # find critical bandwidth of M distribution
-  h.crit <- find.h.crit(M)
+  h.crit <- find.h.crit(M, h.crit.search.depth)
   # bootstrap the critical bandwidth by sampling from the kernel density
   # estimate of M with bandwidth of h.crit
-  M.samples <- bootstrap.sample(M, 1000)
+  M.samples <- bootstrap.sample(M, bootstrap.sample.size)
   M.kernel.samples <- matrix(rnorm(M.samples, mean=M.samples, sd = h.crit), nrow=dim(M.samples)[1], ncol=dim(M.samples)[2])
-  h.crit.bootstrap <- apply(M.kernel.samples, 1, find.h.crit)
+  h.crit.bootstrap <- apply(M.kernel.samples, 1, function(x){ return(find.h.crit(x, h.crit.search.depth)) })
   # check the significance value of the measured critical bandwidth
   p.inv <- sum(h.crit.bootstrap <= h.crit*adjust)/length(h.crit.bootstrap)
   has.outliers <- p.inv >= 1 - alpha
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Bootlier Plots and Automatic Outlier Detection in R
 
 Based on [Candelon & Metiu (2013)](http://www.bundesbank.de/Redaktion/EN/Downloads/Publications/Discussion_Paper_1/2013/2013_02_01_dkp_02.pdf?__blob=publicationFile) and [Singh & Xie (2003)](http://astrostatistics.psu.edu/su13/bootlier.pdf)
+
+`demo.R` contains a working example that replicates the analysis in [Candelon & Metiu (2013)](http://www.bundesbank.de/Redaktion/EN/Downloads/Publications/Discussion_Paper_1/2013/2013_02_01_dkp_02.pdf?__blob=publicationFile)
